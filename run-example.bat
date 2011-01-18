5ce93abeef4871ebc0b6fda6d104dcec78f573b0
Pau Garcia i Quiles <pgquiles@elpauer.org>
2011-01-18 04:22:19 +0100

    More work to make examples runnable

--- run-example.bat
+++ run-example.bat
@@ -3,4 +3,4 @@ REM http://www.microsoft.com/resources/documentation/windows/xp/all/proddocs/en-
 
 set PATH=%~p0;%PATH%
 
-%~p0\..\lib\Wt\examples\%~n0 --docroot ..\lib\Wt\examples\%~n0 --http-port 8080 --http-addr 0.0.0.0
+%~p0\\..\\lib\\Wt\\examples\\@EXAMPLESUBDIR@\\@EXAMPLENAME@.wt  --docroot ..\\lib\\Wt\\examples\\@EXAMPLESUBDIR@ --http-port 8080 --http-addr 0.0.0.0
