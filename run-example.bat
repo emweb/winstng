9c6771df16ce9349e8dc471f85914030afe1ed67
Pau Garcia i Quiles <pgquiles@elpauer.org>
2011-01-19 13:41:07 +0100

    Remove example runners, they are in Wt itself now

--- run-example.bat
+++
@@ -1,6 +0,0 @@
-REM Install in the 'bin' directory
-REM http://www.microsoft.com/resources/documentation/windows/xp/all/proddocs/en-us/percent.mspx?mfr=true
-
-set PATH=%~p0;%PATH%
-
-%~p0\\..\\lib\\Wt\\examples\\@EXAMPLESUBDIR@\\@EXAMPLENAME@.wt  --docroot ..\\lib\\Wt\\examples\\@EXAMPLESUBDIR@ --http-port 8080 --http-addr 0.0.0.0
