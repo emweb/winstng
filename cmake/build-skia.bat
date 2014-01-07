9308e0f8e3b05b65b8f0c682fb3813b16a622e33
Wim Dumon <wim@emweb.be>
2014-01-07 16:11:38 +0100

    Fixes for MSVS2010 and 64bit skia build

--- cmake/build-skia.bat
+++ cmake/build-skia.bat
@@ -1,2 +1,4 @@
 REM could not escape the '=' sign in cmake
-msbuild out/gyp/skia_lib.sln /target:Build /p:Configuration=%1
+REM MSVS2010 installation is easily broken so that msbuild does not work
+REM msbuild out/gyp/skia_lib.sln /target:Build /p:Configuration=%1
+devenv.com out/gyp/skia_lib.sln /Build %1
--- cmake/configure-skia.bat
+++ cmake/configure-skia.bat
@@ -1,4 +1,5 @@
 REM could not escape the '=' sign in cmake
 SET PATH=%1;%PATH%
 cd %3
-%4 %5 %6 %7 %8 %9 -G msvs_version=%2
+SET GYP_DEFINES=skia_arch_width=%4
+%5 %6 %7 %8 %9 -G msvs_version=%2
