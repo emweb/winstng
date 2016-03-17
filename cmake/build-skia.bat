516947c86e6781d9a5400684658a7af78f922aeb
Wim Dumon <wim@emweb.be>
2016-03-17 14:54:52 +0100

    Added missing path for depot tools while building skia (required for ninja)

--- cmake/build-skia.bat
+++ cmake/build-skia.bat
@@ -1,4 +1,5 @@
 REM could not escape the '=' sign in cmake
+SET PATH=%2;%PATH%
 REM MSVS2010 installation is easily broken so that msbuild does not work
 REM msbuild out/gyp/skia_lib.sln /target:Build /p:Configuration=%1
 devenv.com out/gyp/skia_lib.sln /Build %1
