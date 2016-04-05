8e6cb99a673086db11e0e853d1e80053822c7a87
Wim Dumon <wim@emweb.be>
2016-04-05 17:45:33 +0200

    skia 64 bit build fix

--- cmake/configure-skia.bat
+++ cmake/configure-skia.bat
@@ -1,6 +1,7 @@
 REM could not escape the '=' sign in cmake
 SET PATH=%1;%PATH%
 cd %3
-SET GYP_DEFINES=skia_arch_width=%4
+REM old skia uses skia_arch_width (32/64), new skia uses skia_arch_type (x86/x86_64)
+SET GYP_DEFINES=skia_arch_width=%4 skia_arch_type=%5
 REM SET "GYP_GENERATORS=ninja,msvs"
-%5 %6 %7 %8 %9 -G msvs_version=%2
+%6 %7 %8 %9 %10 -G msvs_version=%2
