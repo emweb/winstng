d15735bfc5c3bd00c137f7e3c73038a69100cdaa
Roel Standaert <roel@emweb.be>
2019-06-14 17:24:41 +0200

    Updating versions, formatting, throwing out UNIX stuff
    - Updated OpenSSL to 1.1.1c (hopefully)
    - Updated MariaDB to 3.1.1 (hopefully)
    - Updated asio to 1.12.2 (hopefully)
    - Threw out obsolete things (skia, GraphicsMagick, bzip2, jpeg,...)
    - Set minimum required version to latest stable CMake (we always use the
    latest stable)

--- cmake/configure-skia.bat
+++
@@ -1,7 +0,0 @@
-REM could not escape the '=' sign in cmake
-SET PATH=%1;%PATH%
-cd %3
-REM old skia uses skia_arch_width (32/64), new skia uses skia_arch_type (x86/x86_64)
-SET GYP_DEFINES=skia_arch_width=%4 skia_arch_type=%5
-REM SET "GYP_GENERATORS=ninja,msvs"
-%6 %7 %8 %9 -G msvs_version=%2
