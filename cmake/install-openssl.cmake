7fe12c74faf5722f3667afbe69f3008f91602778
Roel Standaert <roel@emweb.be>
2019-06-17 12:42:01 +0200

    Use cmake scripts again for setting Path

---
+++ cmake/install-openssl.cmake
@@ -0,0 +1,16 @@
+set( _OLDPATH $ENV{PATH} )
+set( _NEWPATH ${WINST_DIR}\\bin ${_OLDPATH} )
+set( ENV{PATH} "${_NEWPATH}" )
+
+message( STATUS "* OpenSSL - Installing..." )
+
+separate_arguments( OPENSSL_INSTALL_COMMAND_WS UNIX_COMMAND "${OPENSSL_INSTALL_COMMAND}" )
+
+execute_process(
+    COMMAND ${OPENSSL_INSTALL_COMMAND_WS}
+    WORKING_DIRECTORY ${OPENSSL_SOURCE_DIR}
+    OUTPUT_FILE ${OPENSSL_SOURCE_DIR}-stamp/openssl-install2-out.txt
+    ERROR_FILE ${OPENSSL_SOURCE_DIR}-stamp/openssl-install2-err.txt
+    OUTPUT_QUIET
+    ERROR_QUIET
+)
